b0fce96c2a7604b6559c3a6e147b65db9bd06510
ShowvenSuresh <showven1201@gmail.com>
2025-02-27 00:06:34 +0800

    disply for login done le

--- src/Views/Login.php
+++ src/Views/Login.php
@@ -4,13 +4,32 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    <title>Document</title>
+    <link href="Styles.css" rel="stylesheet">
+    <title>ToDoApp</title>
 </head>
 
-<body>
+<body class="AuthBody">
+    <div>
+        <form class="form-control" method="POST">
+            <p class="title">To-Do App</p>
+            <p class="title">Login</p>
+            <div class="input-field">
+                <input required="" class="input" type="text" name="email" />
+                <label class="label" for="input">Enter Email</label>
+            </div>
+            <div class="input-field">
+                <input required="" class="input" type="password" name="password" />
+                <label class="label" for="input">Enter Password</label>
+            </div>
+            <input class="submit-btn" type="submit" value="Sign In" name="signin" />
+            <hr>
+            <p>No Account?</p>
+            <a href="./SignUp.php">
+                Sign UP
+            </a>
+        </form>
 
+    </div>
 </body>
 
 </html>
---
+++ src/Views/SignUp.php
@@ -0,0 +1,45 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="Styles.css" rel="stylesheet">
+    <title>ToDoApp</title>
+</head>
+
+<body class="AuthBody">
+    <div>
+        <form class="form-control" action="">
+            <p class="title">Sign Up</p>
+            <div class="input-field">
+                <input required="" class="input" type="text" name="email" />
+                <label class="label" for="input">Email</label>
+            </div>
+            <div class="input-field">
+                <input required="" class="input" type="password" name="password" />
+                <label class="label" for="input">Password</label>
+            </div>
+            <div class="input-field">
+                <input required="" class="input" type="text" name="firstname" />
+                <label class="label" for="input">First Name</label>
+            </div>
+            <div class="input-field">
+                <input required="" class="input" type="text" name="lastname" />
+                <label class="label" for="input">Last Name</label>
+            </div>
+            <div class="input-field">
+                <input required="" class="input" type="text" name="phonenum" />
+                <label class="label" for="input">Phone Number</label>
+            </div>
+            <input class="submit-btn" type="submit" value="Sign Up" name="signup" />
+            <hr>
+            <p>Got Account?</p>
+            <a href="./Login.php">
+                Sign In
+            </a>
+        </form>
+
+    </div>
+</body>
+
+</html>
