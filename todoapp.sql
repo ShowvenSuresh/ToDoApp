42d45b259fcdba0e5f78f3d5ad9cf246b0fe78cd
ShowvenSuresh <showven1201@gmail.com>
2025-03-09 19:48:26 +0800

    added the mysql dump file for the database

---
+++ todoapp.sql
@@ -0,0 +1,88 @@
+-- MySQL dump 10.13  Distrib 8.0.41, for Linux (x86_64)
+--
+-- Host: localhost    Database: todoapp
+-- ------------------------------------------------------
+-- Server version	8.0.41-0ubuntu0.24.04.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `tasks`
+--
+
+DROP TABLE IF EXISTS `tasks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tasks` (
+  `task_id` int NOT NULL AUTO_INCREMENT,
+  `uid` int NOT NULL,
+  `task_name` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `due_date` date NOT NULL,
+  `priority` varchar(255) NOT NULL,
+  `category` varchar(255) NOT NULL,
+  `status` varchar(255) NOT NULL,
+  `isarchive` tinyint(1) NOT NULL,
+  PRIMARY KEY (`task_id`),
+  KEY `uid` (`uid`),
+  CONSTRAINT `tasks_ibfk_1` FOREIGN KEY (`uid`) REFERENCES `users` (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=38 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tasks`
+--
+
+LOCK TABLES `tasks` WRITE;
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
+INSERT INTO `tasks` VALUES (2,2,'PHP individual assignment','need to do loh y want description','2025-04-22','Medium','Assignments & Homework','To-Do',1),(3,1,'wsl install and configuration','help norman','2025-03-03','Low','Assignments & Homework','Doing',0),(4,1,'Complete PHP ','Finish the PHP assignment for practice.','2025-04-25','Low','Assignments & Homework','Completed',1),(5,1,'Prepare for Java Quiz','Review Java topics for the upcoming quiz.','2025-04-26','Medium','Exams & Quizzes','Completed',1),(6,1,'Group Project: Website Redesign','Collaborate with the team to redesign the website.','2025-04-27','High','Projects & Group Work','Completed',1),(8,2,'Write Essay for fun','Draft and finalize the English essay.','2025-05-01','Medium','Personal & Extracurricular','To-Do',0),(9,2,'Study for Math Quiz','Review algebra and geometry topics for the quiz.','2025-05-02','Low','Exams & Quizzes','Doing',0),(10,2,'Team Project: App Development','Work with team members to develop a mobile app.','2025-05-03','High','Projects & Group Work','Completed',0),(11,2,'Join Art Workshop','Participate in a local art workshop to boost creativity.','2025-05-04','Medium','Personal & Extracurricular','Completed',1),(12,3,'Submit Lab Report','Complete and submit the lab report on time.','2025-05-05','High','Assignments & Homework','To-Do',0),(13,3,'Review Chemistry Quiz','Study key concepts for the upcoming chemistry quiz.','2025-05-06','Medium','Exams & Quizzes','Completed',1),(14,3,'Collaborate on Robotics Project','Assist in building and programming the robot.','2025-05-07','Low','Projects & Group Work','Doing',0),(20,2,'Learn solidity','learn the solidity programming language for the hackathon','2025-03-03','High','Projects & Group Work','To-Do',0),(23,3,'PHP individual assignment','fuck this shit','2025-03-04','High','Projects & Group Work','Completed',1),(25,3,'wsl','dddddddd','2025-03-07','Medium','Projects & Group Work','To-Do',0),(26,1,'PHP individual assignment','just test sucesss','2025-03-14','High','Personal & Extracurricular','To-Do',0),(27,1,'hackathon','apubcc hackathon','2025-03-05','Medium','Projects & Group Work','Doing',0),(28,4,'Research History Assignment','Gather information and write the history assignment.','2025-05-09','Medium','Assignments & Homework','Completed',1),(29,4,'Prepare for Geography Quiz','Review maps and geographical facts for the quiz.','2025-03-10','Medium','Exams & Quizzes','To-Do',0),(30,4,'Plan Group Presentation','Organize and prepare slides for the group presentation.','2025-05-11','Low','Projects & Group Work','Completed',1),(31,4,'Enroll in Fitness Class','Sign up for a fitness class to stay active.','2025-05-12','Medium','Personal & Extracurricular','Doing',0),(32,1,'Install ZROK','Help norman install zrok in his computer','2025-03-02','High','Projects & Group Work','Completed',1),(33,4,'Halo','heiheiboy','0025-04-15','Low','Assignments & Homework','Completed',1),(34,1,'Submit Lab ','submmit networking lab','2025-03-18','Medium','Assignments & Homework','Completed',1),(35,4,'Review Chemistry ','do a review for class','2025-03-19','High','Assignments & Homework','Completed',0),(36,4,'test task','this is a testing task','2025-03-10','High','Projects & Group Work','To-Do',0),(37,4,'Submit Lab jjj','ddddddd','2025-03-13','High','Exams & Quizzes','Doing',0);
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+  `uid` int NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) NOT NULL,
+  `phone_num` varchar(20) NOT NULL,
+  PRIMARY KEY (`uid`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'example@gmail.com','naruto1201','Kenji','Chong','+60122127455'),(2,'i23024773@student.newinti.edu.my','showven1201','Thea','Malloc','+60163287455'),(3,'ninetails@example.com','minato','kushina','namikaze','+60163287455'),(4,'toxicaura0@gmail.com','nut123','Jason','Loh the gay','01111542310');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-03-09 19:46:05
